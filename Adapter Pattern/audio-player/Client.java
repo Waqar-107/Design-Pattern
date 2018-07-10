315a70b0c221a88e85ef1110f19789ae03033b67
Waqar-107 <waqar.hassan866@gmail.com>
2018-07-10 21:05:25 +0600

    upd

--- Adapter Pattern/audio-player/Client.java
+++ Adapter Pattern/audio-player/Client.java
@@ -2,9 +2,9 @@ public class Client {
     public static void main(String[] args){
         AudioPlayer audioPlayer = new AudioPlayer();
 
-        audioPlayer.play("beyond the horizon.mp3");
-        audioPlayer.play("alone.mp4");
-        audioPlayer.play("far far away.vlc");
-        audioPlayer.play("mind me.avi");
+        audioPlayer.play("shots.mp3");
+        audioPlayer.play("who we are.mp4");
+        audioPlayer.play("the north remembers.vlc");
+        audioPlayer.play("love me like you do.avi");
     }
 }
